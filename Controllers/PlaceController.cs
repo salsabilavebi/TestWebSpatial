af8ff557d9ea852ba743c7136b3b3e5587597def
salsabilavebi <salsabilavebinatasya19@gmail.com>
2024-01-26 00:56:05 +0700

    web client

---
+++ Controllers/PlaceController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using WebConsumAPI.Models;
+
+namespace WebConsumAPI.Controllers
+{
+    public class PlaceController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7040/api");
+
+        private readonly HttpClient client;
+
+        public PlaceController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/Places/Get").Result;
+
+            if(response.IsSuccessStatusCode)
+            {
+                PlaceViewModel placeResult = response.Content.ReadAsAsync<PlaceViewModel>().Result;
+               
+            }
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(PlaceViewModel model)
+        {
+
+            try
+            {
+                string data = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = client.PostAsync(client.BaseAddress + "/Places/Store", content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["successMessage"] = "Data Ditambahkan";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+            return View(model);
+        }
+
+        
+    }
+}
---
+++ Models/PlaceViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebConsumAPI.Models
+{
+    public class PlaceViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Owner Name")]
+        
+        public string OwnerName { get; set; }
+        [Required]
+        public string placename { get; set; }
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public string placetype { get; set; }
+        [Required]
+        public DateTime date { get; set; }
+
+        public string dateToShortDateString()
+        {
+            return date.ToShortDateString();
+        }
+    }
+
+    
+}
